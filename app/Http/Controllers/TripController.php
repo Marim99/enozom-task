c9c2126884265e48e51427c0b2f7643767118f23
marim99 <marim.khaled99@gmail.com>
2023-06-21 16:53:41 +0300

    enhance

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Station;
 use App\Services\TripService;
 use Illuminate\Http\Request;
+use TheSeer\Tokenizer\Exception;
 
 class TripController extends Controller
 {
@@ -21,29 +22,36 @@ class TripController extends Controller
     public function getTrips(Request $request)
     {
         // Validate the input
-        $request->validate([
-            'from_station' => 'required',
-            'to_station' => 'required',
-        ]);
+    try {
+        
+            $request->validate([
+                'from_station' => 'required',
+                'to_station' => 'required',
+            ]);
 
-        $fromStation = $request->input('from_station');
-        $toStation = $request->input('to_station');
-        $fromStationId = Station::where('id', $fromStation)->value('id');
-        $toStationId = Station::where('id', $toStation)->value('id');
-        $trips = $this->tripService->getTripsByStations($fromStationId, $toStationId);
+            $fromStation = $request->input('from_station');
+            $toStation = $request->input('to_station');
+            $fromStationId = Station::where('id', $fromStation)->value('id');
+            $toStationId = Station::where('id', $toStation)->value('id');
+            $trips = $this->tripService->getTripsByStations($fromStationId, $toStationId);
 
-        if ($trips->isEmpty()) {
-            return response()->json(['No trains for this route'], 404);
+            if ($trips->isEmpty()) {
+                return response()->json(['No trains for this route'], 404);
+            }
+
+            $response = [];
+            foreach ($trips as $trip) {
+                $response[] = [
+                    'id' => $trip->id,
+                    'train_name' => $trip->train->name,
+                ];
+            }
+
+            return response()->json($response);
+
+
+        }catch(Exception $e){
+            return response()->json(['error' => $e->getMessage()], 500);
         }
-
-        $response = [];
-        foreach ($trips as $trip) {
-            $response[] = [
-                'id' => $trip->id,
-                'train_name' => $trip->train->name,
-            ];
-        }
-
-        return response()->json($response);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
+
+
 Route::get('trips', [TripController::class,'getTrips']);
